0722979d57b7e13fb4f941fa133d5f79586cc2e9
Jaroslav Sevcik <jarin.sevcik@gmail.com>
2023-11-13 07:53:05 +0100

    Preparation for jit (trace and tracer)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::sync::Arc;
+
 // Represents a traced value. Wrapper for a float.
 trait Tracer {
     fn add(&self, rhs: &Self) -> Self;
@@ -74,12 +77,6 @@ impl<Inner: Trace> Trace for GradTrace<Inner> {
     }
 }
 
-fn test_fn<T: Trace>(trace: &T, value: &T::Value) -> T::Value {
-    let x_2 = value.mul(value);
-    let x_times_4 = trace.constant(4.0).mul(value);
-    x_2.add(&x_times_4).add(&trace.constant(6.0))
-}
-
 fn grad<T: Trace + Clone, GF>(fun: GF) -> impl Fn(&T, &T::Value) -> T::Value
 where
     GF: Fn(&GradTrace<T>, &<GradTrace<T> as Trace>::Value) -> <GradTrace<T> as Trace>::Value,
@@ -92,6 +89,119 @@ where
         let result = fun(&grad_trace, &value_for_grad);
         result.grad
     }
+}
+
+#[derive(Debug, Clone)]
+struct ExprTracer {
+    // TODO figure out what to do with the cyclic
+    // reference. Perhaps thios should be a reference with
+    // a lifetime at least as long as this object?
+    trace: ExprTrace,
+    value: TracedArgument,
+}
+
+impl Tracer for ExprTracer {
+    fn add(&self, rhs: &Self) -> Self {
+        let index = self
+            .trace
+            .add(TracedExprDef::Add(self.value.clone(), rhs.value.clone()));
+        ExprTracer {
+            trace: self.trace.clone(),
+            value: TracedArgument::Local(index),
+        }
+    }
+
+    fn mul(&self, rhs: &Self) -> Self {
+        let index = self
+            .trace
+            .add(TracedExprDef::Mul(self.value.clone(), rhs.value.clone()));
+        ExprTracer {
+            trace: self.trace.clone(),
+            value: TracedArgument::Local(index),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum TracedArgument {
+    Local(usize),
+    Input(usize),
+}
+
+#[derive(Debug, Clone)]
+enum TracedExprDef {
+    Add(TracedArgument, TracedArgument),
+    Mul(TracedArgument, TracedArgument),
+    Constant(f32),
+}
+
+#[derive(Debug, Clone)]
+struct TracedBlock {
+    program: Vec<TracedExprDef>,
+    outputs: Vec<TracedExprDef>,
+    inputs: usize,
+}
+
+impl TracedBlock {
+    fn new(inputs: usize) -> Self {
+        Self {
+            program: Vec::new(),
+            outputs: Vec::new(),
+            inputs,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ExprTrace {
+    block: Arc<RefCell<TracedBlock>>,
+}
+
+impl ExprTrace {
+    fn new(inputs: usize) -> Self {
+        Self {
+            block: Arc::new(RefCell::new(TracedBlock::new(inputs))),
+        }
+    }
+
+    fn add(&self, item: TracedExprDef) -> usize {
+        let mut block = self.block.borrow_mut();
+        block.program.push(item);
+        block.program.len() - 1
+    }
+}
+
+impl Trace for ExprTrace {
+    type Value = ExprTracer;
+
+    fn constant(&self, value: f32) -> Self::Value {
+        let index = self.add(TracedExprDef::Constant(value));
+        ExprTracer {
+            trace: self.clone(),
+            value: TracedArgument::Local(index),
+        }
+    }
+}
+
+// fn jit<T: Trace + Clone, GF>(fun: GF) -> impl Fn(&T, &T::Value) -> T::Value
+// where
+//     GF: Fn(&ExprTrace, &<ExprTrace as Trace>::Value) -> <ExprTrace as Trace>::Value,
+// {
+//     move |_trace, value| {
+//         // let value_for_grad = GradTracer::<T::Value>::new(value.clone(), trace.constant(1.0));
+//         // let grad_trace = GradTrace {
+//         //     inner: trace.clone(),
+//         // };
+//         // let result = fun(&grad_trace, &value_for_grad);
+//         // result.grad
+//         value.clone()
+//     }
+// }
+
+fn test_fn<T: Trace>(trace: &T, value: &T::Value) -> T::Value {
+    let x_2 = value.mul(value);
+    let x_times_4 = trace.constant(4.0).mul(value);
+    x_2.add(&x_times_4).add(&trace.constant(6.0))
 }
 
 fn main() {
@@ -120,18 +230,18 @@ fn main() {
 #[test]
 fn eval_poly() {
     let x = 3.0;
-    assert_eq!(test_fn(&EvalTrace {}, &x), 27.0); 
+    assert_eq!(test_fn(&EvalTrace {}, &x), 27.0);
 }
 #[test]
 fn eval_grad_poly() {
     let x = 3.0;
     let grad_test_fn = grad::<EvalTrace, _>(test_fn);
-    assert_eq!(grad_test_fn(&EvalTrace {}, &x), 10.0); 
+    assert_eq!(grad_test_fn(&EvalTrace {}, &x), 10.0);
 }
 
 #[test]
 fn eval_grad_grad_poly() {
     let x = 3.0;
     let grad2_test_fn = grad::<EvalTrace, _>(grad::<GradTrace<EvalTrace>, _>(test_fn));
-    assert_eq!(grad2_test_fn(&EvalTrace {}, &x), 2.0); 
+    assert_eq!(grad2_test_fn(&EvalTrace {}, &x), 2.0);
 }
