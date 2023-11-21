a88980cd88e65bb438f295eba2eb8bbddd613964
Jaroslav Sevcik <jarin.sevcik@gmail.com>
2023-11-21 15:53:38 +0100

    Work in progress on jit-of-jit with multiple outputs

--- src/main.rs
+++ src/main.rs
@@ -448,17 +448,17 @@ fn eval_grad_grad_poly() {
 }
 
 #[test]
-fn eval_jit_grad_poly() {
-    let x = 3.0;
-    let jit_of_grad_poly_fn = jit::<EvalTrace, _>(grad::<ExprTrace, _>(poly_fn));
-    assert_eq!(jit_of_grad_poly_fn(&EvalTrace {}, &[x]), [8.0]);
-}
-
-#[test]
 fn eval_jit_poly() {
     let x = 3.0;
     let jit_poly_fn = jit::<EvalTrace, _>(poly_fn);
     assert_eq!(jit_poly_fn(&EvalTrace {}, &[x]), [12.0]);
+}
+
+#[test]
+fn eval_jit_grad_poly() {
+    let x = 3.0;
+    let jit_of_grad_poly_fn = jit::<EvalTrace, _>(grad::<ExprTrace, _>(poly_fn));
+    assert_eq!(jit_of_grad_poly_fn(&EvalTrace {}, &[x]), [8.0]);
 }
 
 #[cfg(test)]
@@ -481,3 +481,27 @@ fn eval_jit_grad_multivar_mul() {
     let jit_of_grad_test_fn = jit::<EvalTrace, _>(grad::<ExprTrace, _>(test_multivar_mul_fn));
     assert_eq!(jit_of_grad_test_fn(&EvalTrace {}, &[x, y]), [4.0, 3.0]);
 }
+
+// #[cfg(test)]
+// fn test_multi_output_fn<T: Trace>(trace: &T, values: &[T::Tracer]) -> Vec<T::Tracer> {
+//     let x2 = trace.mul(&values[0], &values[0]);
+//     let x3 = trace.mul(&x2, &values[0]);
+//     vec![x2, x3]
+// }
+
+// #[cfg(test)]
+// fn test_embed_jit_fn<T: Trace + Clone>(trace: &T, values: &[T::Tracer]) -> Vec<T::Tracer> {
+//     let jitted_multi_out = jit::<T, _>(test_multi_output_fn);
+
+//     let res = jitted_multi_out(trace, &[values[0].clone()]);
+//     let sum = trace.add(&res[0], &res[1]);
+
+//     vec![sum]
+// }
+
+// #[test]
+// fn composed_jit_grad_multivar_mul() {
+//     let x = 3.0;
+//     let jit_composed_fn = jit::<EvalTrace, _>(test_embed_jit_fn);
+//     assert_eq!(jit_composed_fn(&EvalTrace {}, &[x]), [36.0]);
+// }
